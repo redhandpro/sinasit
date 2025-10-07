f2b217bfa5b5dfcd9540d99709a1c85d3b091a9a
redhand <ariaa1968@gmail.com>
2025-10-07 17:25:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// ===== script.js - JS کد شخصی هدر =====
+const customBtn = document.getElementById('custom-btn');
+const customBox = document.getElementById('custom-box');
+
+customBtn.addEventListener('click', () => {
+  alert('دکمه شما کلیک شد!');
+  customBox.style.background = '#6ee7b7a0'; // نمونه تغییر پس‌زمینه
+});
 
